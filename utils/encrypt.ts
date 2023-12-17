26367c66eafc59ecd8d28f8bed7049e0fbc232fc
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-12-17 14:00:24 +0900

    :passport_control: jwt verify function

--- utils/encrypt.ts
+++ utils/encrypt.ts
@@ -3,8 +3,15 @@ import md5 from 'md5';
 
 import * as jwt from 'jsonwebtoken';
 import { config } from '@config/jwt';
+import { z } from 'zod';
 
 const SALT_ROUND = 10;
+
+export type AuthTokenParams = z.infer<typeof AuthenTokenParamsSchema>;
+export const AuthenTokenParamsSchema = z.object({
+  userId: z.string(),
+  email: z.string(),
+});
 
 export const hashString = (source: string): string => {
   return md5(source);
@@ -22,4 +29,16 @@ export const generateJWT = (userId: string, email: string): string => {
   return jwt.sign({ userId, email }, config.secrete, {
     expiresIn: config.expireTime,
   });
+};
+
+export const verifyJWT = (jwtToken: string): [boolean, any, Error] => {
+  let decoded: jwt.JwtPayload | string;
+
+  try {
+    decoded = jwt.verify(jwtToken, config.secrete);
+
+    return [true, decoded, null];
+  } catch (err) {
+    return [false, null, err];
+  }
 };
