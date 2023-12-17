1bce88100ebf12f328024aa77341f0b204be462a
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-12-17 17:50:57 +0900

    :tada: Complete testing for infrastructure layer

---
+++ src/users/infrastructure/rdb-repository/user/testData/index.ts
@@ -0,0 +1,25 @@
+const users = [
+  {
+    id: '0bbfbd4b-8a6b-4f8b-b3f1-4ea728b092cc',
+    email: 'u1@mail.com',
+    name: 'u-1',
+    salt: '$2b$10$NVmx1D6bmEYeMApFJT5Qvu',
+    password: '$2b$10$NVmx1D6bmEYeMApFJT5Qvus2Au3agrQATQ3KZ2dhzqWE1P3DdSJvi',
+  },
+  {
+    id: '0bbfbd4b-8a6b-4f8b-b3f1-4ea728b093cc',
+    email: 'u2@mail.com',
+    name: 'u-2',
+    salt: '$2b$10$NVmx1D6bmEYeMApFJT5Qvu',
+    password: '$2b$10$NVmx1D6bmEYeMApFJT5Qvus2Au3agrQATQ3KZ2dhzqWE1P4DdSJvi',
+  },
+  {
+    id: '0bbfbd4b-8a6b-4f8b-b3f1-4ea728b094cc',
+    email: 'u3@mail.com',
+    name: 'u-3',
+    salt: '$2b$10$NVmx1D6bmEYeMApFJT5Qvu',
+    password: '$2b$10$NVmx1D6bmEYeMApFJT5Qvus2Au3agrQATQ3KZ2dhzqWE1P5DdSJvi',
+  },
+];
+
+export { users };
