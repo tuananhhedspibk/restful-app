9ac024402bc670c7faa27c3cc956931890de4548
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-12-17 12:05:41 +0900

    :goal_net: More error codes and error detail codes

---
+++ libs/exception/application/command/index.ts
@@ -0,0 +1,39 @@
+import { BaseError } from '@libs/exception/base';
+
+export const CommandErrorCode = {
+  BAD_REQUEST: 'BAD_REQUEST',
+  NOT_FOUND: 'NOT_FOUND',
+  INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
+} as const;
+export type CommandErrorCode =
+  (typeof CommandErrorCode)[keyof typeof CommandErrorCode];
+
+export const CommandErrorDetailCode = {
+  EMAIL_ALREADY_EXIST: 'EMAIL_ALREADY_EXIST',
+  EMAIL_CAN_NOT_BE_EMPTY: 'EMAIL_CAN_NOT_BE_EMPTY',
+  PASSWORD_CAN_NOT_BE_EMPTY: 'PASSWORD_CAN_NOT_BE_EMPTY',
+  INVALID_USER_EMAIL: 'INVALID_USER_EMAIL',
+  INVALID_USER_PASSWORD: 'INVALID_USER_PASSWORD',
+  USER_NOT_FOUND: 'USER_NOT_FOUND',
+  PASSWORD_IS_WRONG: 'PASSWORD_IS_WRONG',
+} as const;
+export type CommandErrorDetailCode =
+  (typeof CommandErrorDetailCode)[keyof typeof CommandErrorDetailCode];
+
+type CommandErrorParams = {
+  code: CommandErrorCode;
+  message: string;
+  info?: { [key: string]: any };
+};
+
+export class CommandError extends BaseError {
+  code: CommandErrorCode;
+  info?: { [key: string]: any };
+
+  constructor(params: CommandErrorParams) {
+    super(params.message);
+
+    this.code = params.code;
+    this.info = params.info || this.info;
+  }
+}
