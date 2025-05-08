529a9695ca7392f667dde0af7fd5622579f92471
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2025-05-08 17:30:29 +0900

    :stethoscope: Add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, HealthModule],
 })
 export class AppModule {}
---
+++ src/health/health.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { HealthController } from './presentation/index.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+
+@Module({
+  imports: [TerminusModule, HttpModule],
+  controllers: [HealthController],
+})
+export class HealthModule {}
